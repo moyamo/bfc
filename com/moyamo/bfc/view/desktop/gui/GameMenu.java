9e8fb33fa259ae26e893c942066f4f33c45ded74
Moyamo <moyamodehacker@gmail.com>
2013-12-03 11:48:57 +0200

    Adds server button and IP text field to GameMenu
    Adds server button, IP text field and greys out all components of
    GameMenu except Single Player button since it is the only one that is
    currently functioning.

--- com/moyamo/bfc/view/desktop/gui/GameMenu.java
+++ com/moyamo/bfc/view/desktop/gui/GameMenu.java
@@ -1,8 +1,15 @@
 package com.moyamo.bfc.view.desktop.gui;
 
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.LayoutManager;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import com.moyamo.bfc.Constants;
 
@@ -11,25 +18,38 @@ public class GameMenu extends JFrame{
 	JButton singlePlayer;
 	JButton directIP;
 	JButton multiplayer;
+	JButton server;
+	JTextField ip;
 	
 	public GameMenu() {
 		mainPanel = new JPanel();
 		singlePlayer = new JButton("Single Player");
-		directIP = new JButton("Direct IP");
+		ip = new JTextField(16);
+		directIP = new JButton("Direct IP Connect");
+		server = new JButton("Server");
 		multiplayer = new JButton("Multiplayer");
 				
 		singlePlayer.addActionListener(new GameMenuListener(this));
 		directIP.addActionListener(new GameMenuListener(this));
+		server.addActionListener(new GameMenuListener(this));
 		multiplayer.addActionListener(new GameMenuListener(this));
+	
+		directIP.setEnabled(false);
+		server.setEnabled(false);
+		multiplayer.setEnabled(false);
+		ip.setEnabled(false);
+		ip.setToolTipText("IP Address");
 		
+		mainPanel.setLayout(new FlowLayout());
 		mainPanel.add(singlePlayer);
+		mainPanel.add(ip);
 		mainPanel.add(directIP);
+		mainPanel.add(server);
 		mainPanel.add(multiplayer);
 		
 		this.add(mainPanel);
 		this.setTitle(Constants.GAME_NAME);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		this.setResizable(false);
 		this.pack();
 		this.setVisible(true);
 	}
